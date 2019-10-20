3fd4334a3b05c3b428ab7f5c5ddf44c9b939319e
Vera Fristedt <veran877@student.liu.se>
2019-10-20 15:44:55 +0200

    Stochastic gradient descent done

--- project1/scripts/implementations.py
+++ project1/scripts/implementations.py
@@ -2,6 +2,32 @@
 import numpy as np
 from proj1_helpers import *
 
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+    np.random.seed(1) 
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
 
 def least_squares(y, tx):
     """Calculates the least squares solution."""
@@ -10,7 +36,6 @@ def least_squares(y, tx):
     loss = compute_mse(y, tx, w)
     
     return w, loss
-
 
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
@@ -27,10 +52,49 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     return w, loss
 
 
-def run_gradient_decent(y, tx):
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient from just few examples n and their corresponding y_n labels."""
+    e = y - tx.dot(w)
+    grad = tx.T.dot(e) / (-len(e))
     
+    return grad
+
+
+def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
+    """Stochastic gradient descent algorithm."""
+    loss = 0
+    w = initial_w
+    
+    for n_iter,(y_, tx_) in enumerate(batch_iter(y, tx, batch_size, num_batches=max_iters, shuffle=True)):
+        
+        stoch_gradient = compute_stoch_gradient(y_, tx_, w)
+        loss = compute_mse(y_, tx_, w)
+        w = w - gamma * stoch_gradient
+        
+        #print("Gradient Descent({bi}/{ti}): loss={l}, \nw={w}\n".format(
+              #bi=n_iter, ti=max_iters - 1, l=loss, w=w))
+        
+    return w, loss
+
+
+def run_GD(y, tx):
     max_iters = 500
     gamma = 0.000001
     
     initial_w = np.array([0.0005 for i in range(tx.shape[1])])
     return least_squares_GD(y, tx, initial_w, max_iters, gamma)
+
+def run_SGD(y, tx):
+    # Define the parameters of the algorithm.
+    max_iters = 70
+    gamma = 0.0000001
+    batch_size = 1
+
+    # Initialization
+    w_initial = np.zeros(tx.shape[1])
+
+    # Start SGD.
+    sgd_w, sgd_loss = least_squares_SGD(y, tx, w_initial, batch_size, max_iters, gamma)
+
+    #print('Final loss: ', sgd_loss, '\nFinal weight vector:\n', sgd_w)
+    return sgd_w, sgd_loss
